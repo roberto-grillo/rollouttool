869c25cbc56bd8ea8cb0cf1c87892cd519cb6649
roberto-grillo <116309355+roberto-grillo@users.noreply.github.com>
2025-04-18 15:36:24 +0200

    Delete static/js/theme.js

--- static/js/theme.js
+++
@@ -1,7 +0,0 @@
-// static/js/theme.js
-document.addEventListener('DOMContentLoaded', () => {
-  const theme = localStorage.getItem('theme');
-  if (theme === 'dark') {
-    document.body.classList.add('dark-mode');
-  }
-});
